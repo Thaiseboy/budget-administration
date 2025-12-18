206de0f917bca1b6486f5bb318b0ec9b33a5d566
Get Karanyawad <thaiseboy@gmail.com>
2025-12-18 17:36:31 +0100

    Fix wrong currency for Bath

--- frontend/budget-frontend/src/utils/formatCurrency.ts
+++ frontend/budget-frontend/src/utils/formatCurrency.ts
@@ -1,5 +1,9 @@
-export function formatCurrency(amount: number, currency: 'EUR' | 'USD' | 'GBP' | 'BATH' = 'EUR') {
-    return new Intl.NumberFormat('nl-NL', {
+export function formatCurrency(
+    amount: number,
+    currency: 'EUR' | 'USD' | 'GBP' | 'THB' = 'EUR',
+    locale = 'nl-NL'
+) {
+    return new Intl.NumberFormat(locale, {
         style: "currency",
         currency,
         minimumFractionDigits: 2,
