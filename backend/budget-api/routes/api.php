d17c35fe87e546a44d456228f173df0f8a7f385d
Get Karanyawad <thaiseboy@gmail.com>
2025-12-20 13:32:16 +0100

    Set route for category bud

--- backend/budget-api/routes/api.php
+++ backend/budget-api/routes/api.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TransactionController;
+use App\Http\Controllers\Api\CategoryBudgetController;
 
 Route::get('/transactions', [TransactionController::class, 'index']);
 Route::post('/transactions', [TransactionController::class, 'store']);
 Route::delete('/transactions/{transaction}', [TransactionController::class, 'destroy']);
 Route::get('/transactions/{transaction}', [TransactionController::class, 'show']);
 Route::put('/transactions/{transaction}', [TransactionController::class, 'update']);
+Route::get('/budgets', [CategoryBudgetController::class, 'index']);
+Route::put('/budgets', [CategoryBudgetController::class, 'upsert']);
